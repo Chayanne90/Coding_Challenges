edc958400f49f4da7a947348360935b3fcaed9b8
Chayanne <chayannepaulino@gmail.com>
2019-12-15 17:23:30 -0500

    Coding Challenges java, Data Structure, Algorithms

---
+++ src/data_structure/arrayChallenge1.java
@@ -0,0 +1,26 @@
+package data_structure;
+
+public class arrayChallenge1 {
+
+    /* Challenge1
+
+    Given an array of ints, return true if the array is length 1 or more, and the first element and
+    the last element are equal
+
+    sameFirstLast([1, 2, 3]) → false
+    sameFirstLast([1, 2, 3, 1]) → true
+    sameFirstLast([1, 2, 1]) → true
+
+     */
+
+
+    // Complete the method: sameFirstLast
+    public boolean sameFirstLast(int[] nums) {
+
+        return false;
+    }
+
+    public static void main (String [ ] args){
+
+    }
+}
