7a9f26125f929328bf65cb016574ad4b3226950c
Chayanne <chayannepaulino@gmail.com>
2019-12-27 22:44:14 -0500

    querys

--- src/challenges_java/loopChallenge1.java
+++ src/challenges_java/loopChallenge1.java
@@ -6,19 +6,28 @@ public class loopChallenge1 {
 
     /* Java Loop Challenge2  from HackerRank. BY Chayannne */
 
-    public static void main (String [] argh) {
+    public static void main (String [] args) {
 
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
+        int t = in.nextInt(); // amount of query
 
-        for(int i = 0; i < t; i++){
+        for(int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
 
+            //querys
+            for(int x = 0; x <= n; x ++) {
+
+
+            }
+
+            
+
+
         }
+
         in.close();
 
     }
-
 }
