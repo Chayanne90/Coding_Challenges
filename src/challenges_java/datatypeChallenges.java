77f1a4b13778df6502fa67996344d73885bd031f
Chayanne <chayannepaulino@gmail.com>
2019-12-30 19:28:24 -0500

    DataType method

--- src/challenges_java/datatypeChallenges.java
+++ src/challenges_java/datatypeChallenges.java
@@ -1,11 +1,28 @@
 package challenges_java;
 
+import java.util.Scanner;
+
 public class datatypeChallenges {
 
-    /* java dataType challenges: By chayanne */
 
+    /* java dataType challenges: By chayanne */
     public static void main (String [] args) {
 
-        
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        try {
+            long n = sc.nextLong();
+            System.out.println(n+ " Can;t fitted in: ");
+
+            if (n >= -128 && n <= 127) System.out.println("* byte");
+
+        } catch (Exception e) {
+
+            System.out.println(sc.next() + "can't be found anywhere.");
+        }
+
+
     }
 }
