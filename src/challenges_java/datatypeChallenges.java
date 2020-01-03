20b1caf6a768989e32b00307f06d3cb92714f00c
Chayanne <chayannepaulino@gmail.com>
2020-01-02 21:13:37 -0500

    Datatype challenge Completed

--- src/challenges_java/datatypeChallenges.java
+++ src/challenges_java/datatypeChallenges.java
@@ -17,20 +17,15 @@ public class datatypeChallenges {
             try {
 
                 long n = sc.nextLong();
-                System.out.println(n+ " Can fitted in: ");
+                System.out.println(n+ " Can fitted in:");
 
                 if (n >= -128 && n <= 127) System.out.println("* byte");
 
                 if (n >= -32768 && n <= 32767) System.out.println("* short");
 
-                int num1 = (int)Math.pow(2,31)-1;
+                if (n >= -Math.pow(2,31) &&  n <= Math.pow(2,31)) System.out.println("* int");
 
-                if (n >= -(int)Math.pow(2,31) &&  n <= num1) System.out.println("* int");
-
-                int num2 = (int)Math.pow(2,63)-1;
-
-                if (n >= -(int)Math.pow(2,63) &&  n <= num2) System.out.println("* long");
-
+                if (n <= -Math.pow(2,64) &&  n >= Math.pow(2,64)-1) System.out.println("* long");
 
             } catch (Exception e) {
 
