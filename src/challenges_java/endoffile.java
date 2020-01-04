3566ecb1f10475c2724e0192abd2fee0a8fb13b5
Chayanne <chayannepaulino@gmail.com>
2020-01-03 22:19:56 -0500

    end of file using scanner input

--- src/challenges_java/endoffile.java
+++ src/challenges_java/endoffile.java
@@ -1,11 +1,14 @@
 package challenges_java;
 
+import java.util.Scanner;
+
 public class endoffile {
 
     /*End of file challenge by Chayanne*/
 
     public static void main (String [ ] args) {
-     
+
+        Scanner in = new Scanner()
 
     }
 }
