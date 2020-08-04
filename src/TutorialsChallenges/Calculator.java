014000e111708ee002a51e30fd8021f1a3bfe733
Chayanne Paulino <chayanne@Chayannes-MacBook-Pro.local>
2020-08-03 23:41:43 -0400

    Calculator class

--- src/TutorialsChallenges/Calculator.java
+++ src/TutorialsChallenges/Calculator.java
@@ -1,4 +1,23 @@
 package TutorialsChallenges;
 
 public class Calculator {
+
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+
+        this.carpet = carpet;
+        this.floor = floor;
+    }
+
+    public double getTotalCost() {
+
+        double total =  carpet.getCost() * floor.getArea();
+        return total;
+
+    }
+
+
 }
