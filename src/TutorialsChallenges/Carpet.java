9666ad4d36ccd2c10ed1342bbe00943748fdc72b
Chayanne Paulino <chayanne@Chayannes-MacBook-Pro.local>
2020-08-03 23:41:18 -0400

    Carpet class

--- src/TutorialsChallenges/Carpet.java
+++ src/TutorialsChallenges/Carpet.java
@@ -1,4 +1,23 @@
 package TutorialsChallenges;
 
 public class Carpet {
+
+    private double cost;
+
+    public Carpet(double cost) {
+
+        if (cost < 0) {
+
+            this.cost = 0.0;
+
+        }else {
+
+            this.cost = cost;
+        }
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
+
 }
