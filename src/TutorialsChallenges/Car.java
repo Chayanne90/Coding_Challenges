9b1f68c3850b4dff3cf2deb1178adf7dd5163bac
chayanne19 <chayannepaulino@gmail.com>
2020-08-12 20:12:01 -0700

    Car class

---
+++ src/TutorialsChallenges/Car.java
@@ -0,0 +1,35 @@
+package TutorialsChallenges;
+
+public class Car extends Vehicle  {
+
+    private int wheels;
+    private int door;
+    private int gear;
+    private boolean isManual;
+    private int currentGear;
+
+    public Car(String brand, String type, int wheels, int door, int gear, boolean isManual) {
+        super(brand, type);
+        this.wheels = wheels;
+        this.door = door;
+        this.gear = gear;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Gear Change to " +this.currentGear);
+    }
+
+    public void changeSpeed(int speed, int direction) {
+
+        move(speed,direction);
+        System.out.println("Speed "+speed+ " direction" + direction);
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
