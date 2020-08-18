14ae7c7822d522c27f040822dd71616469f71507
chayanne19 <chayannepaulino@gmail.com>
2020-08-17 19:47:48 -0700

    motherboard class PC component

---
+++ src/TutorialsChallenges/composition/motherboard.java
@@ -0,0 +1,63 @@
+package TutorialsChallenges.composition;
+
+public class motherboard {
+
+    private String model;
+    private String manufacture;
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public motherboard(String model, String manufacture, int ramSlots, int cardSlots, String bios) {
+        this.model = model;
+        this.manufacture = manufacture;
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName) {
+
+        System.out.println("Program "+programName+ " is now loading...");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public int getRamSlots() {
+        return ramSlots;
+    }
+
+    public void setRamSlots(int ramSlots) {
+        this.ramSlots = ramSlots;
+    }
+
+    public int getCardSlots() {
+        return cardSlots;
+    }
+
+    public void setCardSlots(int cardSlots) {
+        this.cardSlots = cardSlots;
+    }
+
+    public String getBios() {
+        return bios;
+    }
+
+    public void setBios(String bios) {
+        this.bios = bios;
+    }
+}
