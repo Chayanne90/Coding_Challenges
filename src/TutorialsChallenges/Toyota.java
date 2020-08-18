ed41080719b659e517418064d652c297a45df6b8
chayanne19 <chayannepaulino@gmail.com>
2020-08-17 19:34:15 -0700

    Toyota class Updated

--- src/TutorialsChallenges/Toyota.java
+++ src/TutorialsChallenges/Toyota.java
@@ -6,8 +6,30 @@ public class Toyota extends Car {
 
     private int roadService;
 
-    public Toyota(int wheels, int door, int gear, boolean isManual, int roadService) {
-        super("4Runner", "TRD", 4, 4, 6, false);
+    public Toyota(int roadService) {
+        super("4Runner", "TRD", 5, 5, 6, false);
         this.roadService = roadService;
+    }
+
+    public void accelerate(int rate) {
+
+        int newVelocity = getCurrentSpeed() + rate;
+
+        if (newVelocity == 0){
+            stop();
+        }else if (newVelocity > 0 && newVelocity <=10){
+            changeGear(1);
+        }else if (newVelocity > 10 && newVelocity <=20){
+            changeGear(2);
+        }else if (newVelocity > 20 && newVelocity <=30){
+            changeGear(3);
+        }else {
+            changeGear(4);
+        }
+
+        if (newVelocity >0) {
+            changeSpeed(newVelocity, getCurrentDirection());
+        }
+
     }
 }
