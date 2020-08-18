e95f5137c64c180d6df940a7aef8db27913c946d
chayanne19 <chayannepaulino@gmail.com>
2020-08-17 19:41:04 -0700

    Monitr class [composition]

---
+++ src/TutorialsChallenges/composition/Monitor.java
@@ -0,0 +1,52 @@
+package TutorialsChallenges.composition;
+
+public class Monitor {
+
+    private String model;
+    private String manufacture;
+    private int size;
+    private Resolution nativeResolution;
+
+    public Monitor(String model, String manufacture, int size, Resolution nativeResolution) {
+        this.model = model;
+        this.manufacture = manufacture;
+        this.size = size;
+        this.nativeResolution = nativeResolution;
+    }
+
+    public void drawPixelAlt(int x, int y, String color) {
+        System.out.println("Drawing pixel at "+ x +","+ y +" in color "+ color);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Resolution getNativeResolution() {
+        return nativeResolution;
+    }
+
+    public void setNativeResolution(Resolution nativeResolution) {
+        this.nativeResolution = nativeResolution;
+    }
+}
