b24cbc051784ca7e5de84266c2dbf01869e2a85b
chayanne19 <chayannepaulino@gmail.com>
2020-08-18 14:48:24 -0700

    bed room component

---
+++ src/TutorialsChallenges/composition/Bed.java
@@ -0,0 +1,43 @@
+package TutorialsChallenges.composition;
+
+public class Bed {
+
+    private String Style;
+    private int pillows;
+    private int height;
+    private int sheets;
+    private int quilt;
+
+    public Bed(String style, int pillows, int height, int sheets, int quilt) {
+        Style = style;
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheets;
+        this.quilt = quilt;
+    }
+
+    public String getStyle() {
+        return Style;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getQuilt() {
+        return quilt;
+    }
+
+    public void make(){
+        System.out.println("Bed -> Making");
+    }
+
+}
