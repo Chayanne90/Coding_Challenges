81247e8dd0f8feb824ca674d438a24b6f57b0d76
chayanne19 <chayannepaulino@gmail.com>
2020-08-19 19:50:27 -0700

    App class [polomorphism challenge]

---
+++ src/TutorialsChallenges/polymorphism/App.java
@@ -0,0 +1,88 @@
+package TutorialsChallenges.polymorphism;
+import TutorialsChallenges.composition.Case;
+
+class Car {
+
+    private boolean Engiene;
+    private int Cylender;
+    private String name;
+
+    public Car(int cylender, String name) {
+        this.Engiene = true;
+        this.Cylender = cylender;
+        this.name = name;
+    }
+
+    public int getCylender() {
+        return Cylender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void StartEngiene(){
+        System.out.println(" Is on");
+    }
+
+    public void accelerate(){
+        System.out.println("Car acelerating");
+    }
+    public void brake(){
+        System.out.println("Car Braking...");
+    }
+}
+
+class Toyota extends Car {
+
+    public Toyota(int cylender, String name) {
+        super(cylender, name);
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("4Runner is accelerating...");
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("4Runner Braking....");
+    }
+}
+
+class MercedezBenz extends Car {
+
+    public MercedezBenz(int cylender, String name) {
+        super(cylender, name);
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("CL 500 is accelerating...");
+    }
+
+    @Override
+    public void brake() {
+        System.out.println("CL 500 Braking....");
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota(4, "4Runner");
+        System.out.print(toyota.getName());
+        toyota.StartEngiene();
+        toyota.accelerate();
+        toyota.brake();
+
+        Nissan nissan = new Nissan(6, "Murano");
+        System.out.print(nissan.getName());
+        nissan.StartEngiene();
+        nissan.accelerate();
+        nissan.brake();
+
+
+    }
+}
