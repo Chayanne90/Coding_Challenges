68f36bfd77d797430e91cb2a5985f50edef928b1
chayanne19 <chayannepaulino@gmail.com>
2020-08-20 19:37:36 -0700

    BillsBurgger

---
+++ src/TutorialsChallenges/polymorphism/BillsBurger.java
@@ -0,0 +1,27 @@
+package TutorialsChallenges.polymorphism;
+
+public class BillsBurger {
+
+    public static void main(String[] args) {
+
+//        Hamburger hamburger = new Hamburger("white","chicken","basic", 5.00);
+//        //hamburger.itemizeHamburger();
+//        hamburger.addAddition1("Bacon", 2.00);
+////        hamburger.addAddition2("Tomato", 1.50);
+////        hamburger.addAddition3("Lettuce", 1.00);
+//        hamburger.addAddition4("Cheese", 3.00);
+//
+//        System.out.println("the total is "+  hamburger.itemizeHamburger());
+
+        healthyBurger healthyBurger = new healthyBurger(" brown "," turky "," veggy ", 10.00);
+
+        healthyBurger.addAddition1("Lettuce", 6.80);
+        healthyBurger.addAddition2("Tomato",2.50);
+        healthyBurger.addHeathyAddition1("Bacon",3.00);
+        healthyBurger.addHeathyAddition2("lightMayo",0.50);
+        System.out.println("the total is "+  healthyBurger.itemizeHamburger());
+
+
+
+    }
+}
