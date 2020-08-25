1303302a73affeade9c2ae958e846a1e08631edf
chayanne19 <chayannepaulino@gmail.com>
2020-08-24 19:22:14 -0700

    print arr method implemented

--- src/TutorialsChallenges/ArraysandListsBoxing/App2.java
+++ src/TutorialsChallenges/ArraysandListsBoxing/App2.java
@@ -7,7 +7,6 @@ public class App2 {
     public static Scanner in = new Scanner(System.in);
 
     /* Get Integers */
-
     public int[] getArray(int size) {
         System.out.println("Please enter the number: .\r");
         int [] arr  = new int[size];
@@ -17,7 +16,15 @@ public class App2 {
         }
         return arr;
     }
-    
+
+    /* print array */
+    public static void printArr(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index: " + i + " -> " + arr[i]);
+        }
+    }
+
     public static void main(String[] args) {
 
     }
