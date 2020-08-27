c6851dafb830b93032d9c0d5ec1cf142c6126ae6
chayanne19 <chayannepaulino@gmail.com>
2020-08-27 10:26:03 -0700

    main class updated

--- src/TutorialsChallenges/ArraysandListsBoxing/main.java
+++ src/TutorialsChallenges/ArraysandListsBoxing/main.java
@@ -1,12 +1,12 @@
 package TutorialsChallenges.ArraysandListsBoxing;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
 
     private static Scanner in = new Scanner(System.in);
     private static GroceryList groceryList = new GroceryList();
-
 
     public static void main(String[] args) {
         boolean quit = false;
@@ -17,7 +17,7 @@ public class main {
             choice = in.nextInt();
             in.nextLine();
 
-            switch (choice){
+            switch (choice) {
                 case 0:
                     printInstrutions();
                     break;
@@ -35,6 +35,12 @@ public class main {
                     break;
                 case 5:
                     searchForItem();
+                    break;
+                case 6:
+                    processList();
+                    break;
+                case 7:
+                    quit = false;
                     break;
             }
         }
@@ -74,10 +80,17 @@ public class main {
     public static void searchForItem() {
         System.out.println("Enter the item number");
         String searchItem = in.nextLine();
-        if (groceryList.findItem(searchItem) != null) {
+        if (groceryList.onFile(searchItem)) {
             System.out.println("Found "+ searchItem+" in our grocery list");
         } else {
             System.out.println(searchItem + " is not in the list");
         }
+    }
+
+    public static void processList() {
+        /*ArrayList<String> newArrayList = new ArrayList<String>();
+        newArrayList.addAll(groceryList.getGroceryList());*/
+
+        ArrayList<String> anotherArrayliostcopy = new ArrayList<String>(groceryList.getGroceryList());
     }
 }
