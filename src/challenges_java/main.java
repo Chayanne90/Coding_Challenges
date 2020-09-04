8c4334413102e2a5c0312c871af64d3f8450d0f8
chayanne19 <chayannepaulino@gmail.com>
2020-09-03 21:59:56 -0700

    Warmup-1 > loneTeen

--- src/challenges_java/main.java
+++ src/challenges_java/main.java
@@ -41,10 +41,24 @@ public class main {
        return ((a >= 13 && a<= 19) || (b >= 13 && b <= 19) || (c >= 13 &&  c <=19));
    }
 
+    /*Warmup-1 > loneTeen*
+    We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+    */
+    public static boolean loneTeen(int a, int b) {
+
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+    }
+
+
+
+
     public static void main(String[] args) {
 
 
-        System.out.println(hasTeen(13,20,10));
+        System.out.println(loneTeen(15,15));
     }
 
 }
