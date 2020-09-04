723424f7716214f4a3270509f0b436faf2558da4
chayanne19 <chayannepaulino@gmail.com>
2020-09-03 20:32:01 -0700

    Warmup-1 > hasTeen from conding bat

--- src/challenges_java/main.java
+++ src/challenges_java/main.java
@@ -27,12 +27,24 @@ public class main {
         }
     }
 
+    /* Warmup-1 > in1020
+    Given 2 int values, return true if either of them is in the range 10..20 inclusive.*/
 
+    public static boolean in1020(int a, int b) {
+        return ((a >= 10 && a <= 20) || (b >= 10 && b <= 20));
+    }
 
+   /* Warmup-1 > hasTeen
+    We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.*/
+   public static boolean hasTeen(int a, int b, int c) {
+
+       return ((a >= 13 && a<= 19) || (b >= 13 && b <= 19) || (c >= 13 &&  c <=19));
+   }
 
     public static void main(String[] args) {
 
-        System.out.println(icyHot(-1,120));
+
+        System.out.println(hasTeen(13,20,10));
     }
 
 }
