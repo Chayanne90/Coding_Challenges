ad3e8d918dd38f58a6274538e8a72093d0ffa489
chayanne19 <chayannepaulino@gmail.com>
2020-09-05 13:47:58 -0700

    Warmup-1 > startOz

--- src/challenges_java/main.java
+++ src/challenges_java/main.java
@@ -84,6 +84,27 @@ public class main {
         return false;
     }
 
+    /* Warmup-1 > startOz
+    *Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o'
+    *and include the second only if it is 'z', so "ozymandias" yields "oz".
+    */
+
+    public static String startOz(String str) {
+
+        String result = "";
+
+        if (str.length() >= 1 && str.charAt(0)=='o') {
+            result = result + str.charAt(0);
+        }
+
+        if (str.length() >= 2 && str.charAt(1)=='z') {
+            result = result + str.charAt(1);
+        }
+
+        return result;
+
+    }
+
 
 
 
@@ -91,7 +112,7 @@ public class main {
     public static void main(String[] args) {
 
 
-        System.out.println(mixStart("ni"));
+        System.out.println(startOz(""));
     }
 
 }
