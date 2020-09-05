b8cbce897eb337aac85c7bbbfabdae79e4d5d3f8
chayanne19 <chayannepaulino@gmail.com>
2020-09-05 16:09:25 -0700

    Warmup-1 > in3050

--- src/challenges_java/main.java
+++ src/challenges_java/main.java
@@ -149,12 +149,24 @@ public class main {
         }
     }
 
+    /* Warmup-1 > in3050
+    * Given 2 int values, return true if they are both in the range 30..40 inclusive,
+    * or they are both in the range 40..50 inclusive.
+    */
+
+    public static boolean in3050(int a, int b) {
+
+        return (((a >= 30 && a <= 40) && (b >= 30 && b <= 40)) || ((a >= 40 && a <= 50) && (b >= 40 && b <= 50)));
+
+    }
+
+
 
 
     public static void main(String[] args) {
 
 
-        System.out.println(close10(20,13));
+        System.out.println(in3050(30,41));
     }
 
 }
