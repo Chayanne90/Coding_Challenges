559d48e56865cac35ac995f13669a48fd60356f7
chayanne19 <chayannepaulino@gmail.com>
2020-09-09 20:36:49 -0700

    Warmup-1 > lastDigit

--- src/challenges_java/main.java
+++ src/challenges_java/main.java
@@ -202,12 +202,23 @@ public class main {
         }
     }
 
+    /* Warmup-1 > lastDigit
+    * Given two non-negative int values, return true if they have the same last digit,
+    * such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+    */
+
+    public static boolean lastDigit(int a, int b) {
+
+        return(a % 10 == b % 10);
+    }
+
+
 
 
 
     public static void main(String[] args) {
 
-        System.out.println(stringE("Heeell"));
+        System.out.println(lastDigit(17,7));
     }
 
 }
