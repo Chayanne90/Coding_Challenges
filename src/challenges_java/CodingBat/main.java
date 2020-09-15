cc7eea116dcfdaafd9fb37006d701ad31aa20612
chayanne19 <chayannepaulino@gmail.com>
2020-09-14 19:36:38 -0700

    Warmup-2 > frontTimes

--- src/challenges_java/CodingBat/main.java
+++ src/challenges_java/CodingBat/main.java
@@ -276,6 +276,30 @@ public class main {
         return strnew;
     }
 
+    /* Warmup-2 > frontTimes
+    * Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever
+    * is there if the string is less than length 3. Return n copies of the front;
+    */
+
+    public static String frontTimes(String str, int n) {
+
+        StringBuilder st = new StringBuilder();
+        String newStr = "";
+
+        if (str.length() < 3) {
+            for (int i = 0; i < n; i++) {
+                st.append(str);
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                st.append(str.substring(0,3));
+            }
+        }
+        newStr = st.toString();
+        return newStr;
+    }
+
+
 
 
 
@@ -288,7 +312,7 @@ public class main {
 
     public static void main(String[] args) {
 
-        System.out.println(stringTimes("hi", 3));
+        System.out.println(frontTimes("Abc", 3));
     }
 
 }
