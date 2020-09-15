035c541086eb1fbddbbb3edf550def2dc038161b
chayanne19 <chayannepaulino@gmail.com>
2020-09-14 21:48:46 -0700

    Warmup-2 > doubleX

--- src/challenges_java/CodingBat/main.java
+++ src/challenges_java/CodingBat/main.java
@@ -314,10 +314,26 @@ public class main {
          return amount;
     }
 
+    /* Warmup-2 > doubleX
+    *  Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+    */
+
+    public static boolean doubleX(String str) {
+
+        int i = str.indexOf("x");
+        if (i == -1) {
+            return false;
+        }
+
+        if (i+1 >= str.length()){
+            return false;
+        }
+        return str.substring(i+1, i+2).equals("x");
+    }
 
     public static void main(String[] args) {
 
-        System.out.println(countXX("xxx"));
+        System.out.println(doubleX("axaxax"));
     }
 
 }
