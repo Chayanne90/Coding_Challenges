5b27de495bd5447e9c36b0e353bd8725bc5c94a9
chayanne19 <chayannepaulino@gmail.com>
2020-09-15 19:57:08 -0700

    Warmup-2 > stringSplosion

--- src/challenges_java/CodingBat/main.java
+++ src/challenges_java/CodingBat/main.java
@@ -348,6 +348,20 @@ public class main {
         return newstr;
     }
 
+    /* Warmup-2 > stringSplosion
+    *  Given a non-empty string like "Code" return a string like "CCoCodCode".
+    */
+
+    public static String stringSplosion(String str) {
+
+        String result = "";
+        for (int i=0; i<str.length(); i++) {
+            result = result + str.substring(0, i+1);
+        }
+        return result;
+
+    }
+
 
 
 
