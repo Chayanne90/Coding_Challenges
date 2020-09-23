f830146e975bbaaf0f675f005c42f167e348a34d
chayanne19 <chayannepaulino@gmail.com>
2020-09-22 22:17:11 -0700

    1. Two Sum- solution not optimize

--- src/challenges_java/LeetCode/Ease.java
+++ src/challenges_java/LeetCode/Ease.java
@@ -40,14 +40,55 @@ public class Ease {
 
     public static String defangIPaddr(String address) {
 
-        address = address.replaceAll("[^a-zA-Z0-9]", "[.]");
-        return address;
+        return address.replaceAll("[^a-zA-Z0-9]", "[.]");
 
+    }
+
+    /* 1470. Shuffle the Array
+    *  Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+    *  Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+    */
+
+    /*public static int[] shuffle(int[] nums, int n) {
+
+    }*/
+
+    /* 1. Two Sum
+    * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    * You can return the answer in any order.
+    * Input: nums = [2,7,11,15], target = 9
+    * Output: [0,1]
+    * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+    */
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int [] newArr = new int[2];
+        int temp = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[j] + nums[i] == target && j != i) {
+                    newArr[0] = j;
+                    newArr[1] = i;
+                    break;
+                }
+            }
+        }
+        return newArr;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(defangIPaddr("255.100.50.0"));
+        int [] arr = {3,0,4,9,6};
+        int target = 10;
+        int [] arr2 =  twoSum(arr,target);
+        for (int i: arr2) {
+
+            System.out.println(i+ " ");
+
+        }
 
     }
 
