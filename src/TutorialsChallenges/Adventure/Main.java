4f65e5a7bf4332af212c0cf98a5dad3ae3a24cfb
chayanne19 <chayannepaulino@gmail.com>
2020-09-24 23:19:12 -0700

    Adventure game lcoations directions

--- src/TutorialsChallenges/Adventure/Main.java
+++ src/TutorialsChallenges/Adventure/Main.java
@@ -15,7 +15,34 @@ public class Main {
         locations.put(0, new Location(0, "You are sitting in front of a computer learning java"));
         locations.put(1, new Location(1, "You are getting better with coding"));
         locations.put(2, new Location(2, "This is Magic"));
-        locations.put(0, new Location(0, "Testing this mapping thing"));
+        locations.put(3, new Location(3, "Testing this mapping thing"));
+        locations.put(4, new Location(4, "You are in a valley besides s stream"));
+        locations.put(5, new Location(5, "You are in the forest"));
+
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 5);
+        locations.get(1).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+        locations.get(5).addExit("Q", 0);
+
+
+
+
 
         int loc = 1;
         while (true) {
