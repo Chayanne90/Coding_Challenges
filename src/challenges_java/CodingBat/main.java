73ef2cb7945ba4de2d9e8f15aa4e8668442788d1
chayanne19 <chayannepaulino@gmail.com>
2020-09-27 14:38:52 -0700

    String-1 > extraEnd

--- src/challenges_java/CodingBat/main.java
+++ src/challenges_java/CodingBat/main.java
@@ -593,18 +593,35 @@ public class main {
         return out.substring(0,2)+word+out.substring(2,out.length());
     }
 
+    /* String-1 > extraEnd
+    *  Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+    * The string length will be at least 2.
+    */
 
+    public static String extraEnd(String str) {
 
+        String newStr ="";
 
+        if (str.length() < 2) {
 
+            for(int i =0; i < 3; i++) {
+                newStr = newStr+ str;
+            }
+        } else {
 
+            for(int i =0; i < 3; i++) {
+                newStr = newStr+ str.substring(str.length()-2, str.length());
+            }
+        }
+        return newStr;
 
+    }
 
 
 
     public static void main(String[] args) {
 
-        System.out.println(makeOutWord("<<>>", "yay"));
+        System.out.println(extraEnd("hello"));
 
 
     }
