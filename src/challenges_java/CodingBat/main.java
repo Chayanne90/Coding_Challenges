5cc1a3d64ff2408c37340eaaced16066717534cc
chayanne19 <chayannepaulino@gmail.com>
2020-09-29 23:23:14 -0700

    Map-1 > mapAB3

--- src/challenges_java/CodingBat/main.java
+++ src/challenges_java/CodingBat/main.java
@@ -890,6 +890,21 @@ public class main {
         return map;
     }
 
+    /*Map-1 > mapAB3
+    * Modify and return the given map as follows: if exactly one of the keys "a" or "b" has a
+    *  value in the map (but not both), set the other to have that same value in the map.
+    */
+
+    public static Map<String, String> mapAB3(Map<String, String> map) {
+
+        if (map.containsKey("a") && !(map.containsKey("b") && map.containsKey("a"))){
+            map.put("b",map.get("a"));
+        } else if (map.containsKey("b") && !(map.containsKey("b") && map.containsKey("a"))) {
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
+
 
 
 
