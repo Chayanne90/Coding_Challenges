dbe4686b45c87c76f7148e2ac3b24dff17734454
chayanne19 <chayannepaulino@gmail.com>
2020-09-29 22:34:36 -0700

    Map-1 > mapShare

--- src/challenges_java/CodingBat/main.java
+++ src/challenges_java/CodingBat/main.java
@@ -799,7 +799,24 @@ public class main {
         return map;
     }
 
-    
+    /* Map-1 > mapShare
+    *  Modify and return the given map as follows: if the key "a" has a value, set the key "b"
+    *  to have that same value. In all cases remove the key "c", leaving the rest of the map
+    *  unchanged.
+    */
+
+    public static Map<String, String> mapShare(Map<String, String> map) {
+
+        if (map.containsKey("a")) {
+
+            map.put("b",map.get("a"));
+        }
+        map.remove("c");
+        return map;
+    }
+
+
+
 
 
 
