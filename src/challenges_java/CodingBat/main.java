53955b20fbbe8c94dda47012935ac24bba5f6ff0
chayanne19 <chayannepaulino@gmail.com>
2020-09-29 23:03:38 -0700

    Map-1 > topping3

--- src/challenges_java/CodingBat/main.java
+++ src/challenges_java/CodingBat/main.java
@@ -853,15 +853,31 @@ public class main {
 
         if (map.containsKey("ice cream")) {
             map.put("yogurt", map.get("ice cream"));
-
         }
-
-        if (map.containsKey("spinach")) {
+        if (map.containsKey("salad")) {
             map.put("spinach", "nuts");
         }
+        return map;
+    }
+
+    /* Map-1 > topping3
+    *  Given a map of food keys and topping values, modify and return the map as follows:
+    *  if the key "potato" has a value, set that as the value for the key "fries". If the
+    *  key "salad" has a value, set that as the value for the key "spinach".
+    */
+
+    public static Map<String, String> topping3(Map<String, String> map) {
+        if (map.containsKey("potato")) {
+            map.put("fries", map.get("potato"));
+        }
+        if (map.containsKey("salad")) {
+            map.put("spinach", map.get("salad"));
+        }
+
 
         return map;
     }
+
 
 
 
