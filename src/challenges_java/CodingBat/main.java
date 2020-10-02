7a8b272be2c8443c0d57e1e34f45add0d1c905e5
chayanne19 <chayannepaulino@gmail.com>
2020-10-01 21:26:57 -0700

    Map-1 > mapAB4 and Map-2 > word0

--- src/challenges_java/CodingBat/main.java
+++ src/challenges_java/CodingBat/main.java
@@ -905,13 +905,47 @@ public class main {
         return map;
     }
 
+    /* Map-1 > mapAB4
+    *  Modify and return the given map as follows: if the keys "a" and "b" have values that have
+    *  different lengths, then set "c" to have the longer value. If the values exist and have the
+    *  same length, change them both to the empty string in the map.
+    */
 
+    public static Map<String, String> mapAB4(Map<String, String> map) {
 
+        if(map.containsKey("a") && map.containsKey("b")) if (map.get("a").length() == map.get("b").length()) {
 
+            map.put("a", "");
+            map.put("b", "");
 
+        } else if (map.get("a").length() > map.get("b").length()) {
 
+            map.put("c", map.get("a"));
+        } else {
+            map.put("c", map.get("b"));
+        }
+        return map;
+    }
 
+    /* Map-2 > word0
+    *  Given an array of strings, return a Map<String, Integer> containing a key for every different string in the array,
+    *  always with the value 0. For example the string "hello" makes the pair "hello":0. We'll do more complicated counting
+    *  later, but for this problem the value is simply 0.
+    */
 
+    public static Map<String, Integer> word0(String[] strings) {
+
+        Map<String, Integer> myMap = new HashMap<>();
+
+        for (int i = 0; i < strings.length; i++) {
+
+            myMap.put(strings[i], 0);
+        }
+
+        return myMap;
+    }
+
+    
 
 
 
@@ -923,7 +957,7 @@ public class main {
         mapTest.put("spinach", "dirt");
         mapTest.put("ice cream", "cherry");
 
-        System.out.println(topping2(mapTest));
+        System.out.println(word0(mapTest));
 
     }
 
