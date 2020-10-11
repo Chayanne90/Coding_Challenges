06da11e511143843e5b1955a03715ffcebc71ccd
chayanne19 <chayannepaulino@gmail.com>
2020-10-10 23:15:45 -0700

    Array-1 > firstLast6

---
+++ src/challenges_java/CodingBat/main2.java
@@ -0,0 +1,30 @@
+package challenges_java.CodingBat;
+
+public class main2 {
+
+    /* Array-1 > firstLast6
+    *  Given an array of ints, return true if 6 appears as either the first
+    *  or last element in the array. The array will be length 1 or more.
+    */
+
+    public static boolean firstLast6(int[] nums) {
+
+        boolean result = false;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (((i == 0) && (nums[i] == 6))) {
+                result = true;
+            } else if((i == nums.length-1) && (nums[nums.length-1] == 6)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {13, 6, 1, 2, 3,6};
+        System.out.println(firstLast6(nums));
+
+    }
+}
