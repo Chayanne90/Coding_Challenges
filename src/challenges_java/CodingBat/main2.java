c2caf0de370b6acc115530fb3fa780d5b82cbcd4
chayanne19 <chayannepaulino@gmail.com>
2020-10-17 12:22:34 -0700

    Array-1 > frontPiece

--- src/challenges_java/CodingBat/main2.java
+++ src/challenges_java/CodingBat/main2.java
@@ -398,6 +398,24 @@ public class main2 {
 
     }
 
+    /* Array-1 > frontPiece
+    *  Given an int array of any length, return a new array of its first 2 elements.
+    *  If the array is smaller than length 2, use whatever elements are present.
+    * */
+
+    public static int[] frontPiece(int[] nums) {
+
+        if ( nums.length < 2){
+            return nums;
+        } else {
+            int [] newArr = new int[2];
+
+            newArr[0] = nums[0];
+            newArr[1] = nums[1];
+            return newArr;
+        }
+    }
+
 
     public static void main(String[] args) {
         int [] nums = {1, 2, 3};
