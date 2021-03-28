5a8e7d84ba3db0051c2a3bca129e08ec79684c8e
chayanne19 <chayannepaulino@gmail.com>
2021-03-27 22:57:31 -0400

    String-1 > atFirst

--- src/challenges_java/CodingBat/main2.java
+++ src/challenges_java/CodingBat/main2.java
@@ -631,13 +631,30 @@ public class main2 {
         return false;
     }
 
+    /* String-1 > atFirst
+    *  Given a string, return a string length 2 made of its first 2 chars.
+    *  If the string length is less than 2, use '@' for the missing chars.
+    */
+
+    public static String atFirst(String str) {
+
+        if (str.length() >= 2){
+            return str.substring(0,2);
+        } else if (str.equals("")){
+            return "@@";
+        } else {
+            return str+"@";
+        }
+
+    }
+
 
 
 
     public static void main(String[] args) {
 
-        String str = "xxbadxx";
+        String str = "";
         //hasBad(str);
-        System.out.println(hasBad(str));
+        System.out.println(atFirst(str));
     }
 }
